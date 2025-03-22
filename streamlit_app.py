e96663c030ee15459ab4e6e172c1ac21248fef3d
Theo Bonifay <35040643+SupaFuture@users.noreply.github.com>
2025-03-22 16:23:54 +0000

    First version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
+
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+df = pd.read_json("datas.json", lines=True)
+st.write(df)
